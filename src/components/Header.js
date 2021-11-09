138efb5ad7d85dcf4bb3f81d63eae695583b3af2
Sezer Alagoz <sezeralagoz8@gmail.com>
2021-11-09 22:56:15 +0300

    html and css are prepared

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <header class="header">
+                <h1>todos</h1>
+                <form>
+                    <input 
+                    className="new-todo" 
+                    placeholder="What needs to be done?" autoFocus 
+                    />
+                </form>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
